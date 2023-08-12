209a84efdb2a34a75df6fe34b3ed11dc4132d39d
shashank01123 <shashank01123@gmail.com>
2023-08-12 23:45:44 +0530

    rev

--- app.py
+++ app.py
@@ -16,12 +16,13 @@ model = pickle.loads(response.content)
 
 st.title('Diabetes Prediction App using Machine Learning')
 st.markdown("The model has been trained on a dataset from the 'National Institute of Diabetes and Digestive and Kidney Diseases' and has demonstrated an accuracy of 85%.")
-
+st.markdown('Find the full code on my Github:  https://github.com/imShashanKashyap/Diabetes_Model')
+st.markdown('Visit to know more about me: https://imshashankashyap.github.io/')
 # Collect input features with placeholders and validation checks
 pregnancies = st.number_input('No. of Pregnancies',min_value=0,  max_value=20, step=1, key="pregnancies")
-glucose = st.number_input('Glucose Level', min_value=0.1, key="glucose")
+glucose = st.number_input('Glucose Level (mg/dL)', min_value=0.1, key="glucose")
 bp = st.number_input('Blood Pressure (mm Hg)', min_value=0.1, key="bp")
-skin_thickness = st.number_input('Tricep Skin Thickness (mm)', min_value=0.1, key="skin_thickness")
+skin_thickness = st.number_input('Triceps Skinfold Thickness (mm)', min_value=0.1, key="skin_thickness")
 insulin = st.number_input('Insulin Level (mu U/ml)', min_value=0.1, key="insulin")
 bmi = st.number_input('BMI', min_value=0.1, key="bmi")
 age = st.number_input('Age', min_value=1, max_value=100, step=1, key="age")
